74d575740bed1f26e22711d9453d5905ee64907a
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-19 12:53:33 -0500

    ask and answer works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,21 @@
 class QuestionsController < ApplicationController
   def ask
     # raise
-    @reponse
+    # @reponse
   end
 
   def answer
     if params[:question].present?
-      question = params[:question]
-      if question == 'I am going to work'
-        @reponse = 'Great!'
-      elsif question[question.length - 1] == '?'
-        @reponse = 'Silly question, get dressed and go to work!'
+      @question = params[:question]
+      if @question == 'I am going to work'
+        @answer = 'Great!'
+      elsif @question[@question.length - 1] == '?'
+        @answer = 'Silly question, get dressed and go to work!'
       else
-        @reponse = "I don't care, get dressed and go to work!"
+        @answer = "I don't care, get dressed and go to work!"
       end
     end
-    @reponse
+    @answer
     # raise
   end
 end
