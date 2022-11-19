5bc1c28596073e93241194421aa77f09bcc7dd66
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-19 11:11:53 -0500

    first answer Great works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,21 @@
 class QuestionsController < ApplicationController
   def ask
+    # raise
+    @reponse
+  end
+
+  def answer
+    if params[:question].present?
+      question = params[:question]
+      if question == 'I am going to work'
+        @reponse = 'Great!'
+      elsif question[question.length - 1] == '?'
+        @reponse = 'Silly question, get dressed and go to work!'
+      else
+        @reponse = "I don't care, get dressed and go to work!"
+      end
+    end
+    @reponse
+    # raise
   end
 end
